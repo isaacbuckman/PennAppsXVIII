97afb6d56ebf8be1548adb7344847b81d1bead79
Isaac Buckman <isaacbuckman4@gmail.com>
2018-09-08 13:52:09 -0400

    chaning from penn_id to email

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ people = [Person(100,100,150,150,"steve", 0000), Person(25,62,60,12,"pete",9999)
 def index():
 	return "Hello, World"
 
-#curl --data "lat=10&long=10&dest_lat=25&dest_long=25&penn_id=1234" localhost:5000/upload-status/
+#curl --data "lat=10&long=10&dest_lat=25&dest_long=25&email=1234" localhost:5000/upload-status/
 @application.route('/upload-status/', methods=['POST'])
 def upload_status():
 	lat = float(request.form['lat'])
@@ -18,63 +18,63 @@ def upload_status():
 	dest_lat = float(request.form['dest_lat'])
 	dest_long = float(request.form['dest_long'])
 	name = request.form['name']
-	penn_id = int(request.form['penn_id'])
+	email = int(request.form['email'])
 
 	for person in people:
-		if person.penn_id == penn_id:
+		if person.email == email:
 			person.update(lat, long, dest_lat, dest_long)
 			break
 	else:
-		new_person = Person(lat, long, dest_lat, dest_long, name, penn_id)
+		new_person = Person(lat, long, dest_lat, dest_long, name, email)
 		people.append(new_person)
 
 	return jsonify(success=True)
 
-# http://localhost:5000/get-friend/?penn_id=1234
+# http://localhost:5000/get-friend/?email=1234
 @application.route('/get-friend/', methods=['GET'])
 def get_friend():
-	penn_id = request.args.get('penn_id')
+	email = request.args.get('email')
 
-	if penn_id != None:
-		penn_id = int(penn_id)
+	if email != None:
+		email = int(email)
 	else:
-		return '''<h1>Please include a penn_id in the query</h1>'''
+		return '''<h1>Please include a email in the query</h1>'''
 
 	myself = None
 	others = []
 	for person in people:
-		if person.penn_id == penn_id:
+		if person.email == email:
 			myself = person
 		else:
 			others.append(person)
 
 	if myself == None:
-		return '''<h1>Your penn_id was not found in our database</h1>'''
+		return '''<h1>Your email was not found in our database</h1>'''
 
 	closest_person = distance.closest(myself, others)
 	if closest_person != None:
 		meetup_location = distance.middle(myself, closest_person)
 		people.remove(myself) #if paired, remove myself from database
-		return jsonify(partner_id=closest_person.penn_id,partner_name=closest_person.name,meetup_location=meetup_location)
+		return jsonify(partner_id=closest_person.email,partner_name=closest_person.name,meetup_location=meetup_location)
 	else:
 		return jsonify(success=False)
 
 @application.route('/complete-cancel/', methods=['POST'])
 def complete_cancel():
-	penn_id = int(request.form['penn_id'])
+	email = int(request.form['email'])
 
 	for person in people:
 		print(str(person))
 
 	for person in people:
-		if person.penn_id == penn_id:
+		if person.email == email:
 			people.remove(person)
 			print()
 			for person in people:
 				print(str(person))
 			return jsonify(success=True)
 
-	return '''<h1>Your penn_id was not found in our database</h1>'''
+	return '''<h1>Your email was not found in our database</h1>'''
 
 if __name__ == '__main__':
 	application.run(debug=True)
--- person.py
+++ person.py
@@ -1,5 +1,5 @@
 class Person:
-	def __init__(self, lat, long, dest_lat, dest_long, name, penn_id):
+	def __init__(self, lat, long, dest_lat, dest_long, name, email):
 		self.lat = lat
 		self.long = long
 		self.dest_lat = dest_lat
@@ -7,10 +7,10 @@ class Person:
 		self.coords = (lat, long)
 		self.dest = (dest_lat, dest_long)
 		self.name = name
-		self.penn_id = penn_id
+		self.email = email
 
 	def __str__(self):
-		return ("lat: %i, long: %i, dest_lat: %i, dest_long: %i, name: %s, penn_id: %i" % (self.lat,self.long,self.dest_lat,self.dest_long,self.name,self.penn_id))
+		return ("lat: %i, long: %i, dest_lat: %i, dest_long: %i, name: %s, email: %i" % (self.lat,self.long,self.dest_lat,self.dest_long,self.name,self.email))
 
 	def update(self, lat, long, dest_lat, dest_long):
 		self.lat = lat
