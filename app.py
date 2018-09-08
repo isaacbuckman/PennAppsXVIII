36b3704b6ccbf64354f5245e01aeae3f5bb91130
Isaac Buckman <isaacbuckman4@gmail.com>
2018-09-08 04:06:38 -0400

    working proto

--- app.py
+++ app.py
@@ -9,22 +9,34 @@ people = [Person(100,100,150,150), Person(25,62,60,12)]
 def index():
 	return "Hello, World!"
 
-#http://localhost:5000/get-friend?lat=100&long=100&dest_lat=150&dest_long=50
-@app.route('/get-friend/', methods=['GET','POST'])
-def query():
-	lat = request.args.get('lat')
-	long = request.args.get('long')
+#curl --data "lat=10&long=10&dest_lat=25&dest_long=25" localhost:5000/upload-status/
+@app.route('/upload-status/', methods=['POST'])
+def upload_status():
+	lat = int(request.form['lat'])
+	long = int(request.form['long'])
+	dest_lat = int(request.form['dest_lat'])
+	dest_long = int(request.form['dest_long'])
 
-	dest_lat = request.args.get('dest_lat')
-	dest_long = request.args.get('dest_long')
+	new_person = Person(lat, long, dest_lat, dest_long)
+	people.append(new_person)
+
+	return '''Data successfully added!'''
+
+# http://localhost:5000/get-friend/
+@app.route('/get-friend/', methods=['GET'])
+def get_friend():
+	penn_id = request.args.get('penn_id')
 
 	for person in people:
-		print("AHHHHH")
+		print()
 		print(str(person))
+		print()
 
-	# print("latitude: %" % lat)
-	# print("longitude: %" % long)
-	return '''<h1>latitude: {}<br/>longitude: {}</h1>'''.format(lat,long)
+	string = ""
+	for person in people:
+		string += ("    " + str(person)+ "    <br/>")
+
+	return string
 
 if __name__ == '__main__':
 	app.run(debug=True)
