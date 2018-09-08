bc2e12526795dfc123a0bdedee6c6681b0d3dafa
Isaac Buckman <isaacbuckman4@gmail.com>
2018-09-08 03:20:20 -0400

    person object

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, request
+from person import Person
 
 app = Flask(__name__)
+
+people = [Person(100,100,150,150), Person(25,62,60,12)]
 
 @app.route('/')
 def index():
 	return "Hello, World!"
 
 #http://localhost:5000/get-friend?lat=100&long=100&dest_lat=150&dest_long=50
-@app.route('/get-friend/', methods=['POST'])
+@app.route('/get-friend/', methods=['GET','POST'])
 def query():
 	lat = request.args.get('lat')
 	long = request.args.get('long')
 
 	dest_lat = request.args.get('dest_lat')
 	dest_long = request.args.get('dest_long')
+
+	for person in people:
+		print("AHHHHH")
+		print(str(person))
 
 	# print("latitude: %" % lat)
 	# print("longitude: %" % long)
---
+++ person.py
@@ -0,0 +1,11 @@
+class Person:
+
+	def __init__(self, lat, long, dest_lat, dest_long, penn_id=0000):
+		self.lat = lat
+		self.long = long
+		self.dest_lat = dest_lat
+		self.dest_long = dest_long
+		self.penn_id = penn_id
+
+	def __str__(self):
+		return ("lat: %i, long: %i, dest_lat: %i, dest_long: %i, penn_id: %i" % (self.lat,self.long,self.dest_lat,self.dest_long,self.penn_id))
