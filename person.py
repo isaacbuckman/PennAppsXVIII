a0851ea0a1ebb9f3076e25f9ef5e84ddcf0084f2
Isaac Buckman <isaacbuckman4@gmail.com>
2018-09-08 21:40:31 -0400

    delete-all and get data endpoints

--- person.py
+++ person.py
@@ -10,7 +10,7 @@ class Person:
 		self.email = email
 
 	def __str__(self):
-		return ("lat: %i, long: %i, dest_lat: %i, dest_long: %i, name: %s, email: %i" % (self.lat,self.long,self.dest_lat,self.dest_long,self.name,self.email))
+		return ("lat: %i, long: %i, dest_lat: %i, dest_long: %i, name: %s, email: %s" % (self.lat,self.long,self.dest_lat,self.dest_long,self.name,self.email))
 
 	def update(self, lat, long, dest_lat, dest_long):
 		self.lat = lat
